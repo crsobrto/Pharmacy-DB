b4f900e19ab421bed1c16f04f95192c497ca4dc5
crsobrto <145484928+crsobrto@users.noreply.github.com>
2023-11-13 16:39:15 -0500

    Finished adding data to all tables, excluding Inventory_OUT

--- Pharmacy_script.sql
+++ Pharmacy_script.sql
@@ -2,6 +2,7 @@ DROP DATABASE Pharmacy;
 CREATE DATABASE Pharmacy;
 USE Pharmacy;
 
+
 -- Creating tables
 CREATE TABLE `employee` (
   `EmployeeID` INTEGER NOT NULL,
@@ -14,7 +15,6 @@ CREATE TABLE `employee` (
   PRIMARY KEY (`EmployeeID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
 CREATE TABLE `manufacturer` (
   `ManufacturerID` INTEGER NOT NULL,
   `ManufacturerName` varchar(20) DEFAULT NULL,
@@ -23,20 +23,18 @@ CREATE TABLE `manufacturer` (
   PRIMARY KEY (`ManufacturerID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
 CREATE TABLE `category` (
   `CategoryID` INTEGER NOT NULL,
   `CategoryName` varchar(25) DEFAULT NULL,
   PRIMARY KEY (`CategoryID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
 CREATE TABLE `medications` (
   `SerialNumber` INTEGER NOT NULL,
   `ManufacturerID` INTEGER DEFAULT NULL,
   `MedicationName` varchar(10) DEFAULT NULL,
-  `GenericName` varchar(15) DEFAULT NULL,
-  `Description` varchar(10) DEFAULT NULL,
+  `GenericName` varchar(25) DEFAULT NULL,
+  `Description` varchar(500) DEFAULT NULL,
   `Strength` varchar(5) DEFAULT NULL,
   `ShelfPrice` varchar(10) DEFAULT NULL,
   `ManufacturerPrice` varchar(10) DEFAULT NULL,
@@ -49,16 +47,15 @@ CREATE TABLE `medications` (
   CONSTRAINT `ManufacturerIDNum` FOREIGN KEY (`ManufacturerID`) REFERENCES `manufacturer` (`ManufacturerID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
 CREATE TABLE `batchreports` (
-  `BatchID` varchar(10) NOT NULL,
+  `BatchID` INTEGER NOT NULL,
   `ManufacturerID` INTEGER DEFAULT NULL,
-  `SerialNumber` int(10) DEFAULT NULL,
-  `Time` varchar(10) DEFAULT NULL,
+  `SerialNumber` INTEGER DEFAULT NULL,
+  `Time` varchar(5) DEFAULT NULL,
   `Date` varchar(10) DEFAULT NULL,
-  `Quantity` varchar(10) DEFAULT NULL,
+  `Quantity` INTEGER DEFAULT NULL,
   `TotalCost` varchar(10) DEFAULT NULL,
-  `ExperationDate` varchar(10) DEFAULT NULL,
+  `ExpirationDate` varchar(7) DEFAULT NULL,
   `EmployeeID` INTEGER DEFAULT NULL,
   PRIMARY KEY (`BatchID`),
   KEY `ManufacturerID_idx` (`ManufacturerID`),
@@ -69,25 +66,22 @@ CREATE TABLE `batchreports` (
   CONSTRAINT `SerialNum` FOREIGN KEY (`SerialNumber`) REFERENCES `medications` (`SerialNumber`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
 CREATE TABLE `inventory` (
-  `InventoryID` varchar(10) NOT NULL,
-  `BatchID` varchar(10) DEFAULT NULL,
-  `Stock` varchar(10) DEFAULT NULL,
+  `InventoryID` INTEGER NOT NULL,
+  `BatchID` INTEGER DEFAULT NULL,
+  `Stock` INTEGER DEFAULT NULL,
   PRIMARY KEY (`InventoryID`),
   KEY `BatchID_idx` (`BatchID`),
   CONSTRAINT `BatchID` FOREIGN KEY (`BatchID`) REFERENCES `batchreports` (`BatchID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
 CREATE TABLE `inventory_out` (
-  `InventoryID` varchar(10) NOT NULL,
-  `BatchID` varchar(10) DEFAULT NULL,
+  `InventoryID` INTEGER NOT NULL,
+  `BatchID` INTEGER DEFAULT NULL,
   PRIMARY KEY (`InventoryID`),
   KEY `BatchID_idx` (`BatchID`),
   CONSTRAINT `BatchReportID` FOREIGN KEY (`BatchID`) REFERENCES `batchreports` (`BatchID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 
 CREATE TABLE `patientlist` (
   `PatientID` INTEGER NOT NULL,
@@ -102,15 +96,14 @@ CREATE TABLE `patientlist` (
   PRIMARY KEY (`PatientID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
 CREATE TABLE `purchasehistory` (
-  `PurchaseId` varchar(10) NOT NULL,
+  `PurchaseID` INTEGER NOT NULL,
   `PatientID` INTEGER DEFAULT NULL,
-  `InventoryID` varchar(10) DEFAULT NULL,
+  `InventoryID` INTEGER DEFAULT NULL,
   `SerialID` INTEGER DEFAULT NULL,
   `Date` varchar(10) DEFAULT NULL,
-  `TypeOfOrder` varchar(10) DEFAULT NULL,
-  `Quantity` varchar(10) DEFAULT NULL,
+  `TypeOfOrder` varchar(12) DEFAULT NULL,
+  `Quantity` INTEGER DEFAULT NULL,
   `TotalCost` varchar(10) DEFAULT NULL,
   `EmployeeID` INTEGER DEFAULT NULL,
   PRIMARY KEY (`PurchaseId`),
@@ -126,8 +119,10 @@ CREATE TABLE `purchasehistory` (
 
 
 
+
+
 -- Adding data to tables
-INSERT INTO PatientList VALUES(1020, 'John', 'Freeman', 'M', 36, '02/15/1987', '(470) 965-3841', '141 E Cherry Street', '670-54-2946');
+INSERT INTO PatientList VALUES(1020, 'John', 'Freeman', 'M', 36, '02/15/1987', '(470) 965-3841', '141 East Cherry Street', '670-54-2946');
 INSERT INTO PatientList VALUES(1036, 'Jackie', 'Moon', 'F', 42, '05/22/1981', '(470) 365-4817', '236 Sunset Boulevard', '452-98-1679');
 INSERT INTO PatientList VALUES(1145, 'Charles', 'Fletcher', 'M', 65, '07/16/1958', '(470) 293-5465', '741 Adams Road', '125-32-0163');
 INSERT INTO PatientList VALUES(1063, 'Mary', 'Smith', 'F', 70, '01/23/1953', '(470) 526-4820', '101 Rivocean Drive', '510-63-5819');
@@ -148,8 +143,6 @@ INSERT INTO PatientList VALUES(1245, 'Rachel', 'Ray', 'F', 41, '04/12/1982', '(4
 INSERT INTO PatientList VALUES(1205, 'Katelyn', 'Marsh', 'F', 23, '02/18/2000', '(470) 949-5369', '2014 Angle Road', '760-14-6813');
 INSERT INTO PatientList VALUES(1345, 'Sarah', 'Monroe', 'F', 48, '06/29/1975', '(470) 510-6472', '460 Play Street', '842-35-4073');
 
--- SELECT * FROM PatientList;
-
 INSERT INTO Employee VALUES(5001, 'Marcus', 'Lightfoot', 'M', 25, '03/03/1998', '240 Marilyn Drive');
 INSERT INTO Employee VALUES(5008, 'Carrie', 'Blunt', 'F', 18, '12/28/2005', '701 Kane Street');
 INSERT INTO Employee VALUES(5016, 'Henry', 'Jarrolds', 'M', 32, '05/17/1991', '630 Roy Avenue');
@@ -160,8 +153,6 @@ INSERT INTO Employee VALUES(5053, 'Elizabeth', 'Pines', 'F', 26, '09/14/1997', '
 INSERT INTO Employee VALUES(5041, 'Hope', 'Redmond', 'F', 36, '08/30/1987', '945 Dogwood Circle');
 INSERT INTO Employee VALUES(5072, 'Desmond', 'Phillips', 'M', 40, '01/25/1983', '360 Battle Drive');
 INSERT INTO Employee VALUES(5036, 'Ned', 'Walker', 'M', 45, '06/13/1978', '890 Yellow Road');
-
--- SELECT * FROM Employee;
 
 INSERT INTO Manufacturer VALUES(9044, 'GlaxoSmithKline', '410 Blackwell Street', '(800) 245-1041');
 INSERT INTO Manufacturer VALUES(9037, 'Organon & Co.', '30 Hudson Street', '(551) 430-6000');
@@ -177,12 +168,87 @@ INSERT INTO Manufacturer VALUES(9572, 'Otsuka', '508 Carnegie Center Drive', '(6
 INSERT INTO Manufacturer VALUES(9730, 'Allergan', '5 Giralda Farms', '(862) 261-8700');
 INSERT INTO Manufacturer VALUES(9846, 'Vanda', '2200 Pennsylvania Avenue Northwest', '(202) 734-3400');
 
--- SELECT * FROM Manufacturer;
-
 INSERT INTO Category VALUES(8010, 'Antimigraine Agents');
 INSERT INTO Category VALUES(8045, 'Antidepressants');
 INSERT INTO Category VALUES(8073, 'Cardiovascular Agents');
 INSERT INTO Category VALUES(8023, 'Antibacterials');
 INSERT INTO Category VALUES(8066, 'Bipolar Agents');
 
+INSERT INTO Medications VALUES(7356, 9044, 'Imitrex', 'Sumatriptan', 'For acute treatment of migraines/cluster headaches. | For use by patients 18 years of age and older.', '20MG', '$14.30', '$12.00', 8010);
+INSERT INTO Medications VALUES(7341, 9037, 'Maxalt', 'Rizatriptan', 'For acute treatment of migraines (not for cluster headaches). | For use by adults and children 6-17 years of age.', '10MG', '$6.40', '$5.00', 8010);
+INSERT INTO Medications VALUES(7398, 9042, 'Zomig', 'Zolmitriptan', 'For treating migraines and reducing substances that trigger headache pain. | For use by adults.', '5MG', '$113.00', '$110.00', 8010);
+INSERT INTO Medications VALUES(7841, 9055, 'Marplan', 'Isocarboxazid', 'For treating depression. Not for newly diagnosed patients of depression. | For use by patients 16 years or older.', '10MG', '$307.00', '$304.00', 8045);
+INSERT INTO Medications VALUES(7865, 9033, 'Nardil', 'Phenelzine', 'For treating depression in patients who have tried other medications unsuccessfully. | For use by patients 16 years and older.', '15MG', '$171.00', '$167.00', 8045);
+INSERT INTO Medications VALUES(7832, 9158, 'Parnate', 'Tranylcypromine', 'For treating certain types of depression. | For use by patients 18 years and older.', '10MG', '$86.20', '$84.00', 8045);
+INSERT INTO Medications VALUES(7429, 9321, 'Amoxil', 'Amoxicillin', 'For treating bacterial infections like chest infections and dental abscesses. | For use by adults, teenagers, and children.', '125MG', '$4.90', '$3.50', 8023);
+INSERT INTO Medications VALUES(7486, 9426, 'Flagyl', 'Metronidazole', 'For treating bacterial and parasitic infections. | For use by teenagers and adults.', '250MG', '$2.90', '$2.00', 8023);
+INSERT INTO Medications VALUES(7477, 9165, 'Cipro', 'Ciprofloxacin', 'For killing and preventing the growth of bacteria. | For use by patients aged 1 year and older.', '500MG', '$7.00', '$6.00', 8023);
+INSERT INTO Medications VALUES(7942, 9426, 'Norpace', 'Disopyramide', 'For treating certain types of serious irregular heartbeats and restore normal heartbeats. | For use by adults.', '600MG', '$124.50', '$122.00', 8073);
+INSERT INTO Medications VALUES(7901, 9044, 'Rythmol', 'Propafenone', 'For preventing atrial fibrillation in patientwho suffer from episodic atrial fibrillation who do not have heart disease. | For use by children and adults.', '150MG', '$41.30', '$39.00', 8073);
+INSERT INTO Medications VALUES(7963, 9618, 'Ethmozine', 'Moricizine Hydrochloride', 'For suppressing nonmalignant ventricular arrhythmias. | For use by patients 18 years of age and older.', '250MG', '$50.00', '$48.50', 8073);
+INSERT INTO Medications VALUES(7041, 9572, 'Abilify', 'Aripiprazole', 'For treating several types of mental health conditions, including schizophrenia, bipolar I disorder, autism, and Tourette syndrome. | For use by adults and children 6 years of age and older.', '20MG', '$100.00', '$98.00', 8066);
+INSERT INTO Medications VALUES(7053, 9730, 'Saphris', 'Asenapine', 'For treating schizophrenia. | For use by adults.', '10MG', '$180.00', '$178.00', 8066);
+INSERT INTO Medications VALUES(7088, 9846, 'Fanapt', 'Iloperidone', 'For treating mental/mood disorders, including schizophrenia. | For use by adults.', '8MG', '$256.00', '$254.00', 8066);
+
+INSERT INTO BatchReports VALUES(6326, 9044, 7356, '08:30', '01/12/2023', 200, '$240.00', '06/2024', 5036);
+INSERT INTO BatchReports VALUES(6975, 9037, 7341, '10:10', '01/04/2023', 300, '$150.00', '08/2024', 5072);
+INSERT INTO BatchReports VALUES(6014, 9042, 7398, '09:15', '04/07/2023', 100, '$1100.00', '01/2025', 5041);
+INSERT INTO BatchReports VALUES(6528, 9055, 7841, '13:15', '07/06/2023', 50, '$1520.00', '04/2024', 5061);
+INSERT INTO BatchReports VALUES(6374, 9033, 7865, '14:30', '05/21/2023', 50, '$835.00', '07/2025', 5072);
+INSERT INTO BatchReports VALUES(6832, 9158, 7832, '14:20', '05/18/2023', 130, '$1092.00', '08/2025', 5036);
+INSERT INTO BatchReports VALUES(6147, 9321, 7429, '11:30', '06/28/2023', 280, '$98.00', '01/2026', 5072);
+INSERT INTO BatchReports VALUES(6428, 9426, 7486, '13:35', '11/17/2023', 250, '$50.00', '05/2025', 5041);
+INSERT INTO BatchReports VALUES(6385, 9165, 7477, '15:40', '03/27/2023', 300, '$180.00', '11/2025', 5041);
+INSERT INTO BatchReports VALUES(6995, 9426, 7942, '10:40', '02/16/2023', 100, '$1220.00', '11/2024', 5061);
+INSERT INTO BatchReports VALUES(6727, 9044, 7901, '15:00', '04/21/2023', 340, '$1326.00', '12/2024', 5036);
+INSERT INTO BatchReports VALUES(6234, 9618, 7963, '11:45', '09/14/2023', 280, '$1358.00', '02/2026', 5072);
+INSERT INTO BatchReports VALUES(6518, 9572, 7041, '13:30', '08/24/2023', 100, '$980.00', '03/2025', 5036);
+INSERT INTO BatchReports VALUES(6925, 9730, 7053, '14:25', '05/19/2023', 50, '$890.00', '07/2025', 5072);
+INSERT INTO BatchReports VALUES(6073, 9846, 7088, '09:50', '10/17/2023', 50, '$1270.00', '08/2024', 5072);
+
+INSERT INTO Inventory VALUES(4087, 6326, 1000);
+INSERT INTO Inventory VALUES(4690, 6975, 2000);
+INSERT INTO Inventory VALUES(4325, 6014, 500);
+INSERT INTO Inventory VALUES(4461, 6528, 250);
+INSERT INTO Inventory VALUES(4135, 6374, 300);
+INSERT INTO Inventory VALUES(4168, 6832, 350);
+INSERT INTO Inventory VALUES(4901, 6147, 2500);
+INSERT INTO Inventory VALUES(4365, 6428, 3000);
+INSERT INTO Inventory VALUES(4430, 6385, 1400);
+INSERT INTO Inventory VALUES(4917, 6995, 800);
+INSERT INTO Inventory VALUES(4434, 6727, 1000);
+INSERT INTO Inventory VALUES(4187, 6234, 1200);
+INSERT INTO Inventory VALUES(4205, 6518, 1100);
+INSERT INTO Inventory VALUES(4836, 6925, 700);
+INSERT INTO Inventory VALUES(4157, 6073, 400);
+
+INSERT INTO PurchaseHistory VALUES(2674, 1020, 4836, 7053, '08/14/2023', 'Prescription', 20, '$360.00', 5001);
+INSERT INTO PurchaseHistory VALUES(2901, 1036, 4087, 7356, '01/01/2023', 'Prescription', 20, '$28.60', 5053);
+INSERT INTO PurchaseHistory VALUES(2145, 1145, 4917, 7942, '06/21/2023', 'Prescription', 5, '$62.25', 5024);
+INSERT INTO PurchaseHistory VALUES(2035, 1063, 4434, 7901, '04/14/2023', 'Prescription', 10, '$41.30', 5016);
+INSERT INTO PurchaseHistory VALUES(2468, 1042, 4187, 7963, '08/13/2023', 'Prescription', 10, '$50.00', 5008);
+INSERT INTO PurchaseHistory VALUES(2716, 1326, 4157, 7088, '10/10/2023', 'Prescription', 10, '$256.00', 5005);
+INSERT INTO PurchaseHistory VALUES(2447, 1080, 4430, 7477, '06/20/2023', 'OTC', 25, '$17.50', 5008);
+INSERT INTO PurchaseHistory VALUES(2681, 1247, 4901, 7429, '04/16/2023', 'OTC', 30, '$14.70', 5024);
+INSERT INTO PurchaseHistory VALUES(2342, 1202, 4205, 7041, '03/23/2023', 'Prescription', 15, '$150.00', 5024);
+INSERT INTO PurchaseHistory VALUES(2166, 1018, 4135, 7865, '09/25/2023', 'Prescription', 10, '$171.00', 5001);
+INSERT INTO PurchaseHistory VALUES(2695, 1312, 4325, 7398, '07/16/2023', 'Prescription', 5, '$56.50', 5008);
+INSERT INTO PurchaseHistory VALUES(2940, 1425, 4157, 7088, '02/12/2023', 'Prescription', 5, '$128.00', 5001);
+INSERT INTO PurchaseHistory VALUES(2717, 1067, 4434, 7901, '06/09/2023', 'Prescription', 5, '$20.65', 5053);
+INSERT INTO PurchaseHistory VALUES(2843, 1027, 4168, 7832, '11/03/2023', 'Prescription', 15, '$129.30', 5016);
+INSERT INTO PurchaseHistory VALUES(2241, 1443, 4365, 7486, '02/17/2023', 'OTC', 20, '$5.80', 5001);
+INSERT INTO PurchaseHistory VALUES(2588, 1572, 4690, 7341, '11/04/2023', 'Prescription', 30, '$19.20', 5024);
+INSERT INTO PurchaseHistory VALUES(2974, 1362, 4430, 7477, '11/14/2023', 'OTC', 25, '$17.50', 5016);
+INSERT INTO PurchaseHistory VALUES(2341, 1245, 4461, 7841, '03/29/2023', 'Prescription', 5, '$153.50', 5024);
+INSERT INTO PurchaseHistory VALUES(2141, 1205, 4325, 7398, '05/24/2023', 'Prescription', 5, '$56.50', 5016);
+INSERT INTO PurchaseHistory VALUES(2050, 1345, 4157, 7088, '10/26/2023', 'Prescription', 5, '$128.00', 5053);
+
+-- SELECT * FROM PatientList;
+-- SELECT * FROM Employee;
+-- SELECT * FROM Manufacturer;
 -- SELECT * FROM Category;
+-- SELECT * FROM Medications;
+-- SELECT * FROM BatchReports;
+-- SELECT * FROM Inventory;
+-- SELECT * FROM PurchaseHistory;
+
